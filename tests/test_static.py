db36ab5eea7faf35f900a01212a3ab061ce53eb1
Adam Doussan <doussan.adam@knights.ucf.edu>
2019-03-30 18:58:49 -0400

    updated tests for static class

--- tests/test_static.py
+++ tests/test_static.py
@@ -1,3 +1,4 @@
+
 import unittest
 
 from unchaind import static as unchaind_static
@@ -15,3 +16,12 @@ class StaticTest(unittest.TestCase):
 
     def test__static_truesec__value(self) -> None:
         self.assertAlmostEqual(unchaind_static.truesec[30_002_187], 1.0)
+
+    def test__static_ships__count(self) -> None:
+        self.assertEqual(len(unchaind_static.ships), 497)
+
+    def test__static_ships__can_convert_to_name(self) -> None:
+        self.assertEqual(unchaind_static.ships[582]["name"], "Bantam")
+
+    def test__static_ships__can_convert_to_class(self) -> None:
+        self.assertEqual(unchaind_static.ships[582]["class"], "Frigate")
