3360fa49989e1b70f5f9239d8be15713ab9fd695
Adam Doussan <doussan.adam@knights.ucf.edu>
2019-03-30 18:57:57 -0400

    added system classes and ships to static data class

--- unchaind/static.py
+++ unchaind/static.py
@@ -10,8 +10,8 @@ def load_systems() -> Dict[int, str]:
         os.path.join(os.path.dirname(__file__), "data", "system.txt")
     ) as f:
         for line in f:
-            a, b, _ = line.strip().split("|")
-            systems[int(a)] = b
+            typeID, name, _, _ = line.strip().split("|")
+            systems[int(typeID)] = name
 
     return systems
 
@@ -23,10 +23,26 @@ def load_truesec() -> Dict[int, float]:
         os.path.join(os.path.dirname(__file__), "data", "system.txt")
     ) as f:
         for line in f:
-            a, _, c = line.strip().split("|")
-            truesec[int(a)] = float(c)
+            typeID, _, secStatus, _ = line.strip().split("|")
+            truesec[int(typeID)] = float(secStatus)
 
     return truesec
+
+
+def load_system_classes() -> Dict[int, int]:
+    systemClasses: Dict[int, int] = {}
+
+    with open(
+        os.path.join(os.path.dirname(__file__), "data", "system.txt")
+    ) as f:
+        for line in f:
+            typeID, _, _, systemClass = line.strip().split("|")
+            if systemClass != '':
+                systemClasses[int(typeID)] = int(systemClass)
+            else:
+                systemClasses[int(typeID)] = -1
+
+    return systemClasses
 
 
 def load_connections() -> Dict[int, List[int]]:
@@ -49,6 +65,21 @@ def load_connections() -> Dict[int, List[int]]:
     return connections
 
 
+def load_ships() -> Dict[int, Dict[str, str]]:
+    ships: Dict[int, Dict[str, str]] = {}
+
+    with open(
+        os.path.join(os.path.dirname(__file__), "data", "ships.txt")
+    ) as f:
+        for line in f:
+            typeID, shipName, shipClass = line.strip().split("|")
+            ships[int(typeID)] = {"name": shipName, "class": shipClass}
+
+    return ships
+
+
 systems: Dict[int, str] = load_systems()
 truesec: Dict[int, float] = load_truesec()
+systemClasses: Dict[int, int] = load_system_classes()
 connections: Dict[int, List[int]] = load_connections()
+ships: Dict[int, Dict[str, str]] = load_ships()
